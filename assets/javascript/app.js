a03bfb5522e22a4052383f42452e61c7a5be09e6
Lisa Smithen <smithenlisa@gmail.com>
2019-08-18 18:41:26 -0500

    Four questions for trivia game added including the corresponding gifs

---
+++ assets/javascript/app.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    var options = [
+        {
+            question: "Harry Potter would not exist without her", 
+            choice: ["Sherrie Cronin", "Edith Wharton", "J.K. Rowling", "Elizabeth Blackwell"],
+            answer: 3,
+            photo: "https://media.giphy.com/media/oGD3yIWPUdbri/giphy.mp4"
+         },
+         {
+             question: "Race barriers were broken by her in both tennis and golf", 
+            choice: ["Serena Williams", "Althea Gibson", "Martina Navratilova", "Margaret Court"],
+            answer: 2,
+            photo: "https://media.giphy.com/media/8zT0D36Myf9C0/giphy.mp4"
+         }, 
+         {
+             question: "Media mogul who has had her own tv show, magazine, tv station and school", 
+            choice: ["Ellen", "Tyra Banks", "Barbara Walters", "Oprah" ],
+            answer: 4,
+            photo: "https://media.giphy.com/media/wrTiOAAVJYN5S/giphy.mp4"
+        }, 
+        {
+            question: "She will have you cooking up a feast in 30 minutes or less", 
+            choice: ["Rachael Ray", "Giada de Laurentiis", "Padma Lakshmi", "Julia Child" ],
+            answer: 1,
+            photo: <div style="width:100%;height:0;padding-bottom:56%;position:relative;"><iframe src="https://giphy.com/embed/MB6PonZP3waIoAMwGf" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/rachaelrayshow-funny-rachael-rachelray-MB6PonZP3waIoAMwGf">via GIPHY</a></p>
+        }, 
+        
